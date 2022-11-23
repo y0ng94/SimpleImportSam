4d34e822e3cc8e91d29e4e561739263104bc7104
김태영 <yaba148@gmail.com>
2022-11-23 15:33:46 +0900

    @fix(compile.sh, run.sh) : 쉘 경로 지정 버그 수정

--- shell/unix/run.sh
+++ shell/unix/run.sh
@@ -3,9 +3,9 @@
 JAVA_HOME=/usr/bin
 BASEPATH=/app/xtorm/test/COM_SimpleImportSam_20221102
 CLASSPATH=$BASEPATH/lib/ojdbc8.jar
-CLASSPATH=$CLASSPATH:/lib/logback-core-1.3.4.jar
-CLASSPATH=$CLASSPATH:/lib/logback-classic-1.3.4.jar
-CLASSPATH=$CLASSPATH:/lib/slf4j-api-2.0.1.jar
+CLASSPATH=$CLASSPATH:$BASEPATH/lib/logback-core-1.3.4.jar
+CLASSPATH=$CLASSPATH:$BASEPATH/lib/logback-classic-1.3.4.jar
+CLASSPATH=$CLASSPATH:$BASEPATH/lib/slf4j-api-2.0.1.jar
 CLASSPATH=$CLASSPATH:$BASEPATH/bin
 
 $JAVA_HOME/java -cp $CLASSPATH App $BASEPATH
